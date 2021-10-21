7879b9bf4176ae8ba1860fda7f98fcec3adfe235
Mdshowanhossain <mdshowanhossain11@gmail.com>
2021-10-22 00:22:59 +0600

    MERN_BLOG_SITE

---
+++ src/Components/Home/Header/Header.js
@@ -0,0 +1,30 @@
+import React from "react";
+import { AppBar, Toolbar, Typography } from "@mui/material";
+import { makeStyles } from "@mui/styles";
+
+const useStyles = makeStyles({
+  component: {
+    backgroundColor: "#ffff",
+    color: "#000000",
+    justifyContent: "center",
+    "& > * ": {
+      padding: 20,
+    },
+  },
+  container: {},
+});
+const Header = () => {
+  const classes = useStyles();
+  return (
+    <AppBar>
+      <Toolbar className={classes.component}>
+        <Typography variant="">HOME</Typography>
+        <Typography variant="">ABOUT</Typography>
+        <Typography variant="">CONTACT</Typography>
+        <Typography variant="">LOGIN</Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
